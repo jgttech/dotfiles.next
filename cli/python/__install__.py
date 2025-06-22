1c148005b399b56cafd50360c52cb2d9463afcb7
Jonathon Tech <jgt.tech@protonmail.com>
2025-06-21 20:24:21 -0500

    WIP

--- cli/python/__install__.py
+++ cli/python/__install__.py
@@ -2,4 +2,11 @@ import src.cli as cli
 
 @cli.command("python")
 def main(ctx: cli.Context) -> None:
-    print("Installing")
+    env, conf = ctx.state
+
+    print("[Environment]")
+    print(f"home.....: {env.home}")
+    print(f"bin......: {env.bin}")
+    print(f"cfg......: {env.cfg}\n")
+    print("[Conf]")
+    print(f"packages.: {conf.packages}")
